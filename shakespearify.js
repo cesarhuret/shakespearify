b2851413a8984e7e5bacfec013936616c13d7bd0
cesar <cesar.huret@gmail.com>
2022-11-19 17:34:38 +0000

    new words 📜

--- shakespearify.js
+++ shakespearify.js
@@ -7,15 +7,22 @@ chrome.action.onClicked.addListener(async (tab) => {
 
 const shakespearify = () => {
     const replacements = [
-                ['your', 'thy'],
-                ['Your', 'Thy'],
-                ['you', 'thou'], 
-                ['You', 'Thou'],
-            ]
-            replacements.forEach(element => {
-                document.body.innerHTML = 
-                    document.body.innerHTML.replaceAll(
-                        element[0], element[1]
-                    );    
-            });
+        ['Your', 'Thy'],
+        ['your', 'thy'],
+        ['you', 'thou'], 
+        ['You', 'Thou'],
+        ['think', 'bethink'],
+        ['Think', 'Bethink'],
+        ['Know', 'Knoweth'],
+        ['know', 'knoweth'],
+        ['are', 'art'],
+        ['Are', 'Art'], 
+    ]
+    
+    replacements.forEach(element => {
+        document.body.innerHTML = 
+            document.body.innerHTML.replaceAll(
+                element[0], element[1]
+            );    
+    });
 }
